ccde31cbc797e801334e9843700cdf873e8d0ac1
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2021-07-23 23:44:57 +0300

    added react-router-dom & material-ui

---
+++ git-browser-client/src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import { TextField, Button } from "@material-ui/core"
+
+export const SearchBar = () => {
+    return (
+        <div>
+            <TextField label="Github username" variant="outlined" size="small"/>
+        </div>
+    )
+}
